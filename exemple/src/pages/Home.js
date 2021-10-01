f935986bce8ba535fa227d10ac4569d2e21ee333
Oliver <oliverjgodoy@gmail.com>
2021-10-01 12:14:41 -0300

    added greeting with useEffect

--- exemple/src/pages/Home.js
+++ exemple/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
    View, 
    Text,
@@ -14,14 +14,32 @@ import { ItemList } from '../components/ItemList';
 export function Home() {
    const [newList, setNewList] = useState('');
    const [myList, setMyList] = useState([]);
+   const [greeting, setGreeting] = useState('');
 
    function handleAddNewList() {
       setMyList(oldState => [...oldState, newList]);
    }
 
+   useEffect(() => {
+      const currentHour = new Date().getHours();
+
+      if(currentHour < 12) {
+         setGreeting('Good morning');
+      } else if (currentHour >= 12 && currentHour < 18) {
+         setGreeting('Good afternoon');
+      } else {
+         setGreeting('Good night');
+      }
+   }, [])
+
   return(
    <View style={ styles.container }>
       <Text style={ styles.title }> Welcome, User</Text>
+
+      <Text style={ styles.greetings }>
+         {greeting}
+      </Text>
+      
       <TextInput 
          style={ styles.input } 
          placeholder="Adicionar item"
@@ -34,6 +52,7 @@ export function Home() {
       <Text style={ [styles.title, { marginVertical: 40 }] }>
          My List
       </Text>
+
       
       <FlatList 
          data={myList}
@@ -61,7 +80,7 @@ const styles = StyleSheet.create({
    },
    title: {
       color: '#FFF',
-      fontSize: 18,
+      fontSize: 20,
       fontWeight: 'bold',
    },
    input: {
@@ -72,5 +91,8 @@ const styles = StyleSheet.create({
       marginTop: 30,
       borderRadius: 7,
    },
-
+   greetings: {
+      color: '#FFF',
+      paddingHorizontal: 5,
+   }
 });
